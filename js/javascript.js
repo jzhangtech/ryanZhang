30942e2521e7fe2b82670356488d89ce3bf23e42
Junjie (Ryan) Zhang <jzhang8299@gmail.com>
2016-04-27 13:09:02 -0400

    formatted javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,3 @@
-
 /*
 el=document.getElementById("abc");
 
@@ -13,95 +12,100 @@ function generateWords(){
 
 
 generateWords()*/
-
 var timer = setInterval(generateWords, 100);
-test=0;
-test2=0;
+test = 0;
+test2 = 0;
 var timer2;
 
 function generateWords() {
 
-	var phrase="Developer";
-				
-	var subphrase = phrase.substring(0, test);
-	document.getElementById("typed2").innerHTML=subphrase;
-	test++;
-   	if(test>=phrase.length+1){
-   		clearInterval(timer);
+    var phrase = "Developer";
 
-		test2=0;
-		setTimeout(function() { timer2 = setInterval(generateWords2, 100); }, 2000);
-	}
+    var subphrase = phrase.substring(0, test);
+    document.getElementById("typed2").innerHTML = subphrase;
+    test++;
+    if (test >= phrase.length + 1) {
+        clearInterval(timer);
+
+        test2 = 0;
+        setTimeout(function() {
+            timer2 = setInterval(generateWords2, 100);
+        }, 2000);
+    }
 }
 
 
 function generateWords2() {
 
-	var testarray=["Developer","Gamer","Wizard Apprentice","Dragon Slayer"]
+    var testarray = ["Developer", "Gamer", "Wizard Apprentice", "Dragon Slayer"]
 
 
-	var array=["Geveloper","Gaveloper","Gameloper","Gameloper","Gamer","Gamer per",
-	"Gamer  r","Gamer"]
-	
+    var array = ["Geveloper", "Gaveloper", "Gameloper", "Gameloper", "Gamer", "Gamer per",
+        "Gamer  r", "Gamer"
+    ]
 
 
-    document.getElementById("typed2").innerHTML=array[test2];
+
+    document.getElementById("typed2").innerHTML = array[test2];
     test2++;
-    if(test2>=(array.length)){
-		clearInterval(timer2);
-		
-		test2=0;
-		
-		
-		setTimeout(function() { timer2 = setInterval(generateWords3, 100); }, 2000);
-		
-		
-		
-		//setInterval(generateWords, 100);
-	}
-   
-	
+    if (test2 >= (array.length)) {
+        clearInterval(timer2);
+
+        test2 = 0;
+
+
+        setTimeout(function() {
+            timer2 = setInterval(generateWords3, 100);
+        }, 2000);
+
+
+
+        //setInterval(generateWords, 100);
+    }
+
+
 }
 
 
 function generateWords3() {
 
-	var testarray=["Developer","Gamer","Wizard Apprentice","Dragon Slayer"]
+    var testarray = ["Developer", "Gamer", "Wizard Apprentice", "Dragon Slayer"]
 
 
-	var array=["Wamer","Wimer","Wizer","Wizarr","Wizard","Wizard A",
-	"Wizard Ap","Wizard App","Wizard Appr","Wizard Appren","Wizard Apprent","Wizard Apprenti","Wizard Apprentic","Wizard Apprentice"]
-	
+    var array = ["Wamer", "Wimer", "Wizer", "Wizarr", "Wizard", "Wizard A",
+        "Wizard Ap", "Wizard App", "Wizard Appr", "Wizard Appren", "Wizard Apprent", "Wizard Apprenti", "Wizard Apprentic", "Wizard Apprentice"
+    ]
 
 
-	console.log("array = " + (array.length-1));
-	console.log("test2 = " + test2);
-    document.getElementById("typed2").innerHTML=array[test2];
+
+    console.log("array = " + (array.length - 1));
+    console.log("test2 = " + test2);
+    document.getElementById("typed2").innerHTML = array[test2];
     test2++;
-    if(test2>=(array.length)){
-		clearInterval(timer2);
-		
-		test=0;
-		
+    if (test2 >= (array.length)) {
+        clearInterval(timer2);
 
-		setTimeout(function() { timer = setInterval(generateWords, 100); }, 2000);
-		
-		
-		
-		//setInterval(generateWords, 100);
-	}
-   
-	
+        test = 0;
+
+
+        setTimeout(function() {
+            timer = setInterval(generateWords, 100);
+        }, 2000);
+
+
+
+        //setInterval(generateWords, 100);
+    }
+
+
 }
 
 
 
 
-
-
-$(function(){
-      $("#typed1").typed({
-        strings: ["Hey, How you doing today?","My name is Ryan..."],
+$(function() {
+    $("#typed1").typed({
+        strings: ["Hey, How you doing today?", "My name is Ryan..."],
         typeSpeed: 0
-      });
-  });
+    });
+});
