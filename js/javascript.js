cbf395ef60c9ae8cd97f4b77da92839c2a0d053e
Junjie (Ryan) Zhang <jzhang8299@gmail.com>
2016-06-01 05:40:58 -0400

    refactor typed function to be object oriented

--- js/javascript.js
+++ js/javascript.js
@@ -1,109 +1,76 @@
-/*
-el=document.getElementById("abc");
-
-function generateWords(){
-	var random=Math.random();
-	console.log("test");
-	el.innerHTML=random;
-
-	window.setTimeout(generateWords(), 2000);
-	
-}
+var index = 0;
+var min=0;
+var charAt=0;
+var stringArr = ['Web Developer','Software Developer','Casual Gamer','Wizard Apprentice', 
+'Dragon Slayer' , 'MeteorJS Fan', 'Game of Throne Fan' , 'Lord of the Net', 'Programmer'];
 
 
-generateWords()*/
-var timer = setInterval(generateWords, 100);
-test = 0;
-test2 = 0;
-var timer2;
+setTimeout(function() {
+    timer = setInterval(typed.bind(null,stringArr), 100);
+    }, 1000);
 
-function generateWords() {
 
-    var phrase = "Developer";
-
-    var subphrase = phrase.substring(0, test);
-    document.getElementById("typed2").innerHTML = subphrase;
-    test++;
-    if (test >= phrase.length + 1) {
+function typed(stringArr){
+    try{
+        var base = stringArr[index];
+        var product = stringArr[index+1];
+        var max = (base.length >= product.length ? base.length : product.length);
+        //var smallerString = (base.length < product.length ? base.length : product.length);
+      
+        if(max>=min){
+            if(base.charAt(charAt)!=''){
+                base=setCharAt(base,charAt,product.charAt(charAt));
+                //console.log('first index = '+ stringArr[index]);
+                //console.log('2nd index = '+ stringArr[index+1]);
+              
+                //console.log(max+" min = " +min);
+                
+                document.getElementById("typed2").innerHTML = base;
+                stringArr[index]=base;
+            }else{
+                base=base+product.charAt(charAt);
+                //console.log('first index = ' + stringArr[index]);
+                //console.log('2nd index = ' + stringArr[index+1]);
+               
+                //console.log(max+" min = " +min);
+               
+                document.getElementById("typed2").innerHTML = base;
+                stringArr[index]=base;
+            }
+        }
+        //console.log('Char = '+ charAt);
+        //console.log('Index = '+ index);
+        if(product.charAt(charAt)!=''){
+            charAt++;
+            min--;
+        }
+        min++;
+        //console.log(max+" min = " +min);
+        if(stringArr[index]==stringArr[index+1]){
+            //console.log('first index = '+stringArr[index]);
+            //console.log('2nd index = '+stringArr[index+1]);
+            clearInterval(timer);
+            index++;
+            charAt = 0;
+            min = 0;
+            setTimeout(function() {
+                timer = setInterval(typed.bind(null,stringArr), 100);
+            }, 1000);
+        }
+    }catch(e){
         clearInterval(timer);
-
-        test2 = 0;
-        setTimeout(function() {
-            timer2 = setInterval(generateWords2, 100);
-        }, 2000);
     }
 }
 
-
-function generateWords2() {
-
-    var testarray = ["Developer", "Gamer", "Wizard Apprentice", "Dragon Slayer"]
-
-
-    var array = ["Geveloper", "Gaveloper", "Gameloper", "Gameloper", "Gamer", "Gamer per",
-        "Gamer  r", "Gamer"
-    ]
-
-
-
-    document.getElementById("typed2").innerHTML = array[test2];
-    test2++;
-    if (test2 >= (array.length)) {
-        clearInterval(timer2);
-
-        test2 = 0;
-
-
-        setTimeout(function() {
-            timer2 = setInterval(generateWords3, 100);
-        }, 2000);
-
-
-
-        //setInterval(generateWords, 100);
-    }
-
-
+function setCharAt(str,index,change) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + change + str.substr(index+1);
 }
 
-
-function generateWords3() {
-
-    var testarray = ["Developer", "Gamer", "Wizard Apprentice", "Dragon Slayer"]
-
-
-    var array = ["Wamer", "Wimer", "Wizer", "Wizarr", "Wizard", "Wizard A",
-        "Wizard Ap", "Wizard App", "Wizard Appr", "Wizard Appren", "Wizard Apprent", "Wizard Apprenti", "Wizard Apprentic", "Wizard Apprentice"
-    ]
-
-
-
-    document.getElementById("typed2").innerHTML = array[test2];
-    test2++;
-    if (test2 >= (array.length)) {
-        clearInterval(timer2);
-
-        test = 0;
-
-
-        setTimeout(function() {
-            timer = setInterval(generateWords, 100);
-        }, 2000);
-
-
-
-        //setInterval(generateWords, 100);
-    }
-
-
-}
-
-
-
-
+/*
 $(function() {
     $("#typed1").typed({
         strings: ["Hey, How you doing today?", "My name is Ryan..."],
         typeSpeed: 0
     });
-});
+});*/
---
+++ to-do.md
@@ -0,0 +1,4 @@
+1. Mobile responsiveness
+2. Facebook Graph API for content management
+3. Refactor Methods for typing display  
+4. Refactor Css, get rid of in-line CSS
